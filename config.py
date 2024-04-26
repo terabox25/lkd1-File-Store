5d120394fe6e472f48ae75a90853279967e5f3ed
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-04-26 19:41:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,9 +35,9 @@ API_HASH = environ.get("API_HASH", "bb32e00647b1bfe66e6cd298a2c66a5a")
 ADMINS = int(environ.get("ADMINS", "5018200809"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://terabox255:ykCXKWw8U3sdjBi2@cluster0.ivldf9a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://terabox255:iih4zq4It0iU17h8@cluster0.g5tmjqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://terabox255:iih4zq4It0iU17h8@cluster0.ivldf9a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CDB_NAME = environ.get("CDB_NAME", "terabox255")
+DB_URI = environ.get("DB_URI", "mongodb+srv://terabox255:ykCXKWw8U3sdjBi2@cluster0.g5tmjqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "lkdbotz")
 
 # Don't Remove Credit Tg - @VJ_Botz
